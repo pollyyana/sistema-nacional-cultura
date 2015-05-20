9ee2033c51326072375c091d91b0e60f3b6971f2
root <root@mcsrv14001.minc.intra>
2015-05-20 15:32:37 -0300

    Add Estrutura Settings

--- snc/wsgi.py
+++ snc/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "snc.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "snc.config.settings")
 
 application = get_wsgi_application()
