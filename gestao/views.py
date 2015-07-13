5c93e143cddc480d32457e29d430adf2a17561bd
Gabriel Avaloni <avaloni@users.noreply.github.com>
2015-07-13 16:59:23 -0300

    Campos de busca por nome de município e filtro por situação nas páginas de gestão

--- gestao/views.py
+++ gestao/views.py
@@ -20,6 +20,18 @@ def alterar_situacao(request, id):
 
 
 class AcompanharAdesao(ListView):
-    queryset = Usuario.objects.filter(estado_processo__range=('1', '5'))
     template_name = 'gestao/adesao/acompanhar.html'
     paginate_by = 10
+
+    def get_queryset(self):
+        situacao = self.request.GET.get('situacao', None)
+        ente_federado = self.request.GET.get('municipio', None)
+
+        if situacao and situacao in ('1', '2', '3', '4', '5'):
+            return Usuario.objects.filter(estado_processo=situacao)
+
+        if ente_federado:
+            return Usuario.objects.filter(
+                municipio__cidade__nome_municipio=ente_federado)
+
+        return Usuario.objects.filter(estado_processo__range=('1', '5'))
