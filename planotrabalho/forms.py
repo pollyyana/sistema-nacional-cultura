118142988229e9db648fb135d1bb6d0eff89d988
Gabriel Avaloni <avaloni@users.noreply.github.com>
2015-07-16 14:37:11 -0300

    Mudando a situação dos anexos do plano de trabalho a medida que são executados

--- planotrabalho/forms.py
+++ planotrabalho/forms.py
@@ -32,6 +32,18 @@ class CriarSistemaForm(ModelForm):
 
         return self.cleaned_data['data_final_sancao_lei']
 
+    def save(self, commit=True, *args, **kwargs):
+        sistema = super(CriarSistemaForm, self).save(commit=False)
+        if 'minuta_projeto_lei' in self.changed_data:
+            sistema.situacao_minuta = 1
+
+        if 'lei_sistema_cultura' in self.changed_data:
+            sistema.situacao_lei_sistema = 1
+
+        if commit:
+            sistema.save()
+        return sistema
+
     class Meta:
         model = CriacaoSistema
         exclude = ['situacao_minuta', 'situacao_lei_sistema']
@@ -51,6 +63,15 @@ class OrgaoGestorForm(ModelForm):
                                         str(limite))
 
         return self.cleaned_data['data_final_estruturacao_secretaria']
+
+    def save(self, commit=True, *args, **kwargs):
+        orgao = super(OrgaoGestorForm, self).save(commit=False)
+        if 'relatorio_atividade_secretaria' in self.changed_data:
+            orgao.situacao_relatorio_secretaria = 1
+
+        if commit:
+            orgao.save()
+        return orgao
 
     class Meta:
         model = OrgaoGestor
@@ -72,6 +93,15 @@ class ConselhoCulturalForm(ModelForm):
 
         return self.cleaned_data['data_final_instalacao_conselho']
 
+    def save(self, commit=True, *args, **kwargs):
+        conselho = super(ConselhoCulturalForm, self).save(commit=False)
+        if 'ata_regimento_aprovado' in self.changed_data:
+            conselho.situacao_ata = 1
+
+        if commit:
+            conselho.save()
+        return conselho
+
     class Meta:
         model = ConselhoCultural
         exclude = ['situacao_ata']
@@ -91,6 +121,15 @@ class FundoCulturaForm(ModelForm):
                                         str(limite))
 
         return self.cleaned_data['data_final_instituicao_fundo_cultura']
+
+    def save(self, commit=True, *args, **kwargs):
+        fundo = super(FundoCulturaForm, self).save(commit=False)
+        if 'lei_fundo_cultura' in self.changed_data:
+            fundo.situacao_lei_plano = 1
+
+        if commit:
+            fundo.save()
+        return fundo
 
     class Meta:
         model = FundoCultura
@@ -151,6 +190,27 @@ class PlanoCulturaForm(ModelForm):
                                         str(limite))
 
         return self.cleaned_data['data_final_sancao_lei_plano_cultura']
+
+    def save(self, commit=True, *args, **kwargs):
+        plano = super(PlanoCulturaForm, self).save(commit=False)
+        if 'relatorio_diretrizes_aprovadas' in self.changed_data:
+            plano.situacao_relatorio_diretrizes = 1
+
+        if 'minuta_preparada' in self.changed_data:
+            plano.situacao_minuta = 1
+
+        if 'ata_reuniao_aprovacao_plano' in self.changed_data:
+            plano.situacao_ata = 1
+
+        if 'ata_votacao_projeto_lei' in self.changed_data:
+            plano.situacao_ata_votacao = 1
+
+        if 'lei_plano_cultura' in self.changed_data:
+            plano.situacao_lei_plano = 1
+
+        if commit:
+            plano.save()
+        return plano
 
     class Meta:
         model = PlanoCultura
