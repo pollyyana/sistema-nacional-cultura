011ec259f402542ffd2da655b0a10eadaaa82a77
Gabriel Avaloni <avaloni@users.noreply.github.com>
2015-07-20 17:30:46 -0300

    Acompanhamento da execução das etapas do plano de trabalho

--- gestao/urls.py
+++ gestao/urls.py
@@ -4,10 +4,32 @@ from django.contrib.admin.views.decorators import staff_member_required
 from . import views
 
 urlpatterns = [
+    # Acompanhar andamento dos processos de adesão
     url(r'^$', staff_member_required(
         views.AcompanharAdesao.as_view(),
         login_url='adesao:login'), name='acompanhar_adesao'),
-    url(r'alterar/situacao/(?P<id>[\w]+)$',
+    url(r'^alterar/situacao/(?P<id>[\w]+)$',
         staff_member_required(views.alterar_situacao, login_url='adesao:login'),
         name='alterar_situacao'),
+
+    # Acompanhar criação do sistema de cultura
+    url(r'^acompanhar/sistema$', staff_member_required(
+        views.AcompanharSistema.as_view(),
+        login_url='adesao:login'), name='acompanhar_sistema'),
+    # Acompanhar criação do órgão gestor
+    url(r'^acompanhar/orgao$', staff_member_required(
+        views.AcompanharOrgao.as_view(),
+        login_url='adesao:login'), name='acompanhar_orgao'),
+    # Acompanhar criação do conselho cultural
+    url(r'^acompanhar/conselho$', staff_member_required(
+        views.AcompanharConselho.as_view(),
+        login_url='adesao:login'), name='acompanhar_conselho'),
+    # Acompanhar criação do fundo de cultura
+    url(r'^acompanhar/fundo$', staff_member_required(
+        views.AcompanharFundo.as_view(),
+        login_url='adesao:login'), name='acompanhar_fundo'),
+    # Acompanhar elaboração do plano de cultura
+    url(r'^acompanhar/plano$', staff_member_required(
+        views.AcompanharPlano.as_view(),
+        login_url='adesao:login'), name='acompanhar_plano'),
 ]
--- gestao/views.py
+++ gestao/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect
+from django.http import Http404
 from django.views.generic import ListView
 
 from adesao.models import Usuario
@@ -25,7 +26,7 @@ class AcompanharAdesao(ListView):
         situacao = self.request.GET.get('situacao', None)
         ente_federado = self.request.GET.get('municipio', None)
 
-        if situacao and situacao in ('1', '2', '3', '4', '5'):
+        if situacao in ('1', '2', '3', '4', '5'):
             return Usuario.objects.filter(estado_processo=situacao)
 
         if ente_federado:
@@ -37,3 +38,64 @@ class AcompanharAdesao(ListView):
             return municipio if municipio else estado
 
         return Usuario.objects.filter(estado_processo__range=('1', '5'))
+
+
+class AcompanharSistema(ListView):
+    template_name = 'gestao/planotrabalho/acompanhar_sistema.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        anexo = self.request.GET.get('anexo', None)
+        if not anexo:
+            raise Http404()
+        usuarios = Usuario.objects.filter(estado_processo='6')
+        usuarios = usuarios.exclude(plano_trabalho__criacao_sistema=None)
+        return usuarios
+
+
+class AcompanharOrgao(ListView):
+    template_name = 'gestao/planotrabalho/acompanhar_orgao.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        usuarios = Usuario.objects.filter(estado_processo='6')
+        usuarios = usuarios.exclude(plano_trabalho__orgao_gestor=None)
+        usuarios = usuarios.filter(plano_trabalho__orgao_gestor__situacao_relatorio_secretaria=1)
+        return usuarios
+
+
+class AcompanharConselho(ListView):
+    template_name = 'gestao/planotrabalho/acompanhar_conselho.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        usuarios = Usuario.objects.filter(estado_processo='6')
+        usuarios = usuarios.exclude(plano_trabalho__conselho_cultural=None)
+        usuarios = usuarios.filter(plano_trabalho__orgao_gestor__situacao_ata=1)
+        return usuarios
+
+
+class AcompanharFundo(ListView):
+    template_name = 'gestao/planotrabalho/acompanhar_fundo.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        anexo = self.request.GET.get('anexo', None)
+        if not anexo:
+            raise Http404()
+        usuarios = Usuario.objects.filter(estado_processo='6')
+        usuarios = usuarios.exclude(plano_trabalho__fundo_cultura=None)
+        return usuarios
+
+
+class AcompanharPlano(ListView):
+    template_name = 'gestao/planotrabalho/acompanhar_plano.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        anexo = self.request.GET.get('anexo', None)
+        if not anexo:
+            raise Http404()
+        usuarios = Usuario.objects.filter(estado_processo='6')
+        usuarios = usuarios.exclude(plano_trabalho__plano_cultura=None)
+        return usuarios
