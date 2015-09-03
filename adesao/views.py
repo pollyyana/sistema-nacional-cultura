284733b07457189155de4355c0881009df5fd3c0
Nitai Bezerra da Silva <nitaibezerra@gmail.com>
2015-09-03 17:38:23 -0300

    Melhorando descrição da coluna Localização

--- adesao/views.py
+++ adesao/views.py
@@ -122,7 +122,7 @@ def exportar_xls(request):
     planilha.write(0, 5, 'Bairro')
     planilha.write(0, 6, 'CEP')
     planilha.write(0, 7, 'Telefone')
-    planilha.write(0, 8, 'Local')
+    planilha.write(0, 8, 'Localização do processo')
 
     for i, municipio in enumerate(Municipio.objects.all(), start=1):
         uf = municipio.estado.sigla
