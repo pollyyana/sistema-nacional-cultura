522e7be5efe794150dd9713653943190903cf431
Pedro Philipe Alves de Oliveira <pedro.oliveira2@cultura.gov.br>
2017-04-17 14:07:58 -0300

    Alteração de prorrogazação do prazo

--- gestao/views.py
+++ gestao/views.py
@@ -98,13 +98,14 @@ class AcompanharPrazo(ListView):
         ente_federado = self.request.GET.get('municipio', None)
         if ente_federado:
             municipio = Usuario.objects.filter(
-                municipio__cidade__nome_municipio__icontains=ente_federado)
+                municipio__cidade__nome_municipio__icontains=ente_federado).order_by('municipio__estado__nome_uf')
             estado = Usuario.objects.filter(
                 municipio__cidade__isnull=True,
-                municipio__estado__nome_uf__icontains=ente_federado)
+                municipio__estado__nome_uf__icontains=ente_federado).order_by('municipio__estado__nome_uf')
 
             return municipio | estado
-        return Usuario.objects.filter(estado_processo='6', data_publicacao_acordo__isnull=False)
+        return Usuario.objects.filter(estado_processo='6', data_publicacao_acordo__isnull=False).order_by(
+            'municipio__estado__nome_uf')
 
 
 def aditivar_prazo(request, id):
@@ -112,6 +113,7 @@ def aditivar_prazo(request, id):
         user = Usuario.objects.get(id=id)
         user.prazo = user.prazo + 1
         user.save()
+
     return redirect('gestao:acompanhar_prazo')
 
 
