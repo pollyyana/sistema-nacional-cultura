1a41b8cfb451a5f07ea0b169a0f1292840a743d0
Pedro Philipe Alves de Oliveira <pedro.oliveira2@cultura.gov.br>
2017-05-24 14:38:28 -0300

    Fix criação conselheiro

--- planotrabalho/forms.py
+++ planotrabalho/forms.py
@@ -295,6 +295,10 @@ class CriarConselheiroForm(ModelForm):
 
         return self.cleaned_data['segmento']
 
+    def clean(self):
+        if self.cleaned_data['segmento'] == '21' and self.cleaned_data['outros'] == '':
+            self.add_error('segmento', 'Este campo é obrigatório.')
+
     def save(self, commit=True, *args, **kwargs):
         conselheiro = super(CriarConselheiroForm, self).save(commit=False)
         conselheiro.conselho = self.usuario.plano_trabalho.conselho_cultural
@@ -335,7 +339,6 @@ class AlterarConselheiroForm(ModelForm):
         return self.cleaned_data['segmento']
 
     def clean(self):
-
         if self.cleaned_data['segmento'] == '21' and self.cleaned_data['outros'] == '':
             self.add_error('segmento', 'Este campo é obrigatório.')
 
