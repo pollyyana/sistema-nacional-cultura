12c4e360c7816330a0fcb8489cc9642f0c116c17
Matheus Batista <matheusbattista@hotmail.com>
2017-11-09 15:24:37 -0200

    [UPDATE] Implementa busca em acoesplanotrabalho com os parametros:     - sistema_cultura_nome_municipio     - sistema_cultura_cnpj_prefeitura     - sistema_cultura_id

--- api/views.py
+++ api/views.py
@@ -42,8 +42,32 @@ class MunicipioDetail(generics.RetrieveAPIView):
 class PlanoTrabalhoList(generics.ListAPIView):
     queryset = PlanoTrabalho.objects.filter().order_by('-id')
     serializer_class = PlanoTrabalhoSerializer 
-    
 
+    def get_queryset(self):
+        queryset = PlanoTrabalho.objects.filter().order_by('-id')
+        
+        # Parâmetros de busca passados na requisição 
+        #situacao_acao = self.request.query_params.get('situacao_acao',None)
+        sistema_cultura_id = self.request.query_params.get('sistema_cultura_id',None)
+        sistema_cultura_nome_municipio = self.request.query_params.get(
+                'sistema_cultura_nome_municipio',None)
+        sistema_cultura_cnpj_prefeitura = self.request.query_params.get(
+                'sistema_cultura_cnpj_prefeitura', None)
+
+        search_params = {'usuario__municipio_id': sistema_cultura_id, 
+                'usuario__municipio__cidade__nome_municipio': sistema_cultura_nome_municipio,
+                'usuario__municipio__cnpj_prefeitura': sistema_cultura_cnpj_prefeitura}
+
+        # Lista parâmetros não vazios
+        arguments = {}
+        for key, value in search_params.items():
+            if value:
+                arguments[key] = value
+
+        queryset = PlanoTrabalho.objects.filter(**arguments)
+
+        return queryset
+        
 # Retorna um plano de trabalho especificado pela pk
 class PlanoTrabalhoDetail(generics.RetrieveAPIView):
     queryset = PlanoTrabalho.objects.filter().order_by('-id') 
