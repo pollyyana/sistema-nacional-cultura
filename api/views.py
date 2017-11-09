efa2f420f1becbd2893223090c84e9865c36886f
Matheus Batista <matheusbattista@hotmail.com>
2017-11-09 13:44:38 -0200

    [UPDATE]Implementa busca por nome_muncipio, sigla_estado e cnpj_prefeitura

--- api/views.py
+++ api/views.py
@@ -1,6 +1,5 @@
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
-from drf_hal_json import views as hal_views
 from planotrabalho.models import PlanoTrabalho 
 from adesao.models import Municipio, Cidade, Usuario
 from api.serializers import MunicipioSerializer, UsuarioSerializer, PlanoTrabalhoSerializer
@@ -8,16 +7,34 @@ from api.serializers import MunicipioSerializer, UsuarioSerializer, PlanoTrabalh
 
 # MUNICIPIOS
 # Lista todos os municipios
-class  MunicipioList(hal_views.HalCreateModelMixin,generics.ListAPIView):
+class  MunicipioList(generics.ListAPIView):
     queryset = Municipio.objects.filter().order_by('-id')
     serializer_class = MunicipioSerializer
     
-#    filter_backends = (DjangoFilterBackend,)
-#    filter_fields = ('governo',)
+    def get_queryset(self):
+        queryset = Municipio.objects.filter().order_by('-id')
+
+        # Parâmetros de busca passados na requisição 
+        cnpj_prefeitura = self.request.query_params.get('cnpj_prefeitura',None)
+        sigla_estado = self.request.query_params.get('sigla_estado',None)
+        nome_municipio = self.request.query_params.get('nome_municipio',None)
+
+        search_params = {'cnpj_prefeitura': cnpj_prefeitura, 'estado__sigla': sigla_estado,
+                         'cidade__nome_municipio': nome_municipio}
+
+        # Lista parâmetros não vazios
+        arguments = {}
+        for key, value in search_params.items():
+            if value:
+                arguments[key] = value
+
+        queryset = Municipio.objects.filter(**arguments)
+
+        return queryset
 
 # Retorna um municipio especificado pela pk
 class MunicipioDetail(generics.RetrieveAPIView):
-    queryset = Municipio.objects.filter().order_by('-id') 
+    queryset = Municipio.objects.filter().order_by('-id')
     serializer_class = MunicipioSerializer
 
 # PLANO TRABALHO
@@ -34,7 +51,7 @@ class PlanoTrabalhoDetail(generics.RetrieveAPIView):
 
 # USUÁRIOS
 # Lista todos os usuários
-class UsuarioList(hal_views.HalCreateModelMixin,generics.ListAPIView):
+class UsuarioList(generics.ListAPIView):
     queryset = Usuario.objects.filter().order_by('-id')
     serializer_class = UsuarioSerializer
     
