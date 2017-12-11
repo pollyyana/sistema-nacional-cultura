a531d953d4f4dd4bd8d55abd5e4753de0a840972
André "decko" de Brito <brito.afa@gmail.com>
2017-12-11 11:49:50 -0200

    [UPDATE] Introduz os primeiros testes

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from rest_framework import status
+
+pytestmark = pytest.mark.django_db
+
+def test_municipios_list_endpoint_returning_200_OK(client):
+
+    url = '/v1/sistemadeculturalocal/'
+    host_request = 'api'
+
+    request = client.get(url, HTTP_HOST=host_request)
+
+    assert request.status_code == status.HTTP_200_OK
