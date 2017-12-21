ae025504c00a3e4b49c74dc7c1ca66f21eb921a4
Luiz Pinheiro <luiz.pinheiro@cultura.gov.br>
2017-12-21 14:11:53 -0200

    [UPDATE] Refatora pesquisa em /sistemadeculturalocal e /acoesplanotrabalho     - /acoesplanotrabalho         - Agora pesquisa utiliza só um underline     - /sistemadeculturalocal         -?nome_municipio         -?estado_sigla

--- api/filters.py
+++ api/filters.py
@@ -4,19 +4,10 @@ from planotrabalho.models import *
 from adesao.models import Municipio, Cidade, Usuario, Uf 
 
 # Classes para filtros de pesquisa
-class UfFilter(filters.FilterSet):
-    class Meta:
-        model = Uf 
-        fields = {'sigla'}
-
-class CidadeFilter(filters.FilterSet):
-    class Meta:
-        model = Cidade
-        fields = {'nome_municipio'}
 
 class MunicipioFilter(filters.FilterSet):
-    cidade = filters.RelatedFilter(CidadeFilter, name='cidade', queryset=Cidade.objects.all())
-    estado = filters.RelatedFilter(UfFilter, name='estado', queryset=Uf.objects.all())
+    estado_sigla = filters.CharFilter(name='estado__sigla')
+    nome_municipio = filters.CharFilter(name='cidade__nome_municipio')
     class Meta:
         model = Municipio
         fields = {'id','cnpj_prefeitura'}        
@@ -24,53 +15,28 @@ class MunicipioFilter(filters.FilterSet):
 class SituacoesFilter(filters.FilterSet):
     class Meta:
         model = SituacoesArquivoPlano
-        fields = {'id','descricao',}
-
-class ConselhoCulturalFilter(filters.FilterSet):
-    situacao = filters.RelatedFilter(SituacoesFilter, name='situacao_ata', queryset=SituacoesArquivoPlano.objects.all())
-    class Meta:
-        model = ConselhoCultural
-        fields = {'id',}
-
-class CriacaoSistemaFilter(filters.FilterSet):
-    situacao = filters.RelatedFilter(SituacoesFilter, 
-            name='situacao_lei_sistema', queryset=SituacoesArquivoPlano.objects.all())
-    class Meta:
-        model = CriacaoSistema
-        fields = {'id',}
-
-class FundoCulturaFilter(filters.FilterSet):
-    situacao = filters.RelatedFilter(SituacoesFilter, 
-            name='situacao_lei_plano', queryset=SituacoesArquivoPlano.objects.all())
-    class Meta:
-        model = FundoCultura 
-        fields = {'id',}
-
-class PlanoCulturaFilter(filters.FilterSet):
-    situacao = filters.RelatedFilter(SituacoesFilter, 
-            name='situacao_lei_plano', queryset=SituacoesArquivoPlano.objects.all())
-    class Meta:
-        model = PlanoCultura 
-        fields = {'id',}
-
-class OrgaoGestorFilter(filters.FilterSet):
-    situacao = filters.RelatedFilter(SituacoesFilter, 
-            name='situacao_relatorio_secretaria', queryset=SituacoesArquivoPlano.objects.all())
-    class Meta:
-        model = OrgaoGestor
-        fields = {'id',}
+        fields = {'descricao': ['istartswith'],}
 
 class PlanoTrabalhoFilter(filters.FilterSet):
-    situacao_conselho= filters.RelatedFilter(SituacoesFilter, 
-        name='conselho_cultural__situacao_ata', queryset=ConselhoCultural.objects.all())
-    situacao_orgao = filters.RelatedFilter(SituacoesFilter, 
-        name='orgao_gestor__situacao_relatorio_secretaria', queryset=OrgaoGestor.objects.all())
-    situacao_lei = filters.RelatedFilter(SituacoesFilter, 
-        name='criacao_sistema__situacao_lei_sistema', queryset=CriacaoSistema.objects.all())
-    situacao_fundo = filters.RelatedFilter(SituacoesFilter,
-        name='fundo_cultura__situacao_lei_plano', queryset=FundoCultura.objects.all())
-    situacao_plano = filters.RelatedFilter(SituacoesFilter,
-        name='plano_cultura__situacao_lei_plano', queryset=PlanoCultura.objects.all())
+    situacao_conselho_descricao = filters.CharFilter(name='conselho_cultural__situacao_ata__descricao',
+            lookup_expr='istartswith')
+    situacao_conselho_id = filters.NumberFilter(name='conselho_cultural__situacao_ata__id')
+
+    situacao_orgao_descricao = filters.CharFilter(name='orgao_gestor__situacao_relatorio_secretaria__descricao',
+            lookup_expr='istartswith')
+    situacao_orgao_id = filters.NumberFilter(name='orgao_gestor__situacao_relatorio_secretaria__id')
+
+    situacao_lei_descricao = filters.CharFilter(name='criacao_sistema__situacao_lei_sistema__descricao',
+            lookup_expr='istartswith')
+    situacao_lei_id = filters.CharFilter(name='criacao_sistema__situacao_lei_sistema__id')
+
+    situacao_fundo_descricao = filters.CharFilter(name='fundo_cultura__situacao_lei_plano__descricao',
+            lookup_expr='istartswith')
+    situacao_fundo_id = filters.NumberFilter(name='fundo_cultura__situacao_lei_plano__descricao__id')
+
+    situacao_plano_descricao = filters.CharFilter(name='plano_cultura__situacao_lei_plano__descricao',
+            lookup_expr='istartswith')
+    situacao_plano_id = filters.NumberFilter(name='plano_cultura__situacao_lei_plano__descricao__id')
 
     sistema_cultura = filters.RelatedFilter(MunicipioFilter,
         name='usuario__municipio', queryset=Municipio.objects.all())
