66cb30b3f766b9d6220f6e5ccbd889c366e22f6b
decko <brito.afa@gmail.com>
2018-02-05 12:02:21 -0200

    [WIP] Testa a lista de classificações
    Testa se a lista de classificações é retornada e renderizada conforme as
    especificações
    
    Co-authored-by: Matheus Batista <matheusbattista@hotmail.com>

--- gestao/tests/test_componentes.py
+++ gestao/tests/test_componentes.py
@@ -14,6 +14,15 @@ def engine():
 
     return engine
 
+@pytest.fixture
+def template(engine):
+    """ Injeta o template 'gestao/diligencia/diligencia.html' como um objeto Template
+        pronto para ser usado."""
+
+    template = engine.get_template(template_name='gestao/diligencia/diligencia.html', dirs=engine.dirs)
+
+    return template
+
 def test_existencia_template_diligencia(engine, client):
     """ Testando existência do template para criação da diligência"""
 
@@ -25,12 +34,10 @@ def test_existencia_template_diligencia(engine, client):
     assert isinstance(template, Template)
 
 
-def test_botoes_acao_diligencia_template(engine, client):
+def test_botoes_acao_diligencia_template(template, client):
     """Testa existencia dos botões de enviar e cancelar
     no template de diligência"""
 
-    template = engine.get_template(
-        template_name='gestao/diligencia/diligencia.html', dirs=engine.dirs)
     rendered_template = template.render(Context({}))
 
     # __import__('ipdb').set_trace()
@@ -38,23 +45,17 @@ def test_botoes_acao_diligencia_template(engine, client):
     assert "<button class='btn btn-secondary'>Cancelar</button>" in rendered_template
 
 
-def test_gestao_template(engine, client):
+def test_gestao_template(template, client):
     """Testa se o template da gestão está sendo carregado"""
-
-    template = engine.get_template(
-        template_name='gestao/diligencia/diligencia.html', dirs=engine.dirs)
 
     rendered_template = template.render(Context({}))
 
     assert "<!DOCTYPE html>" in rendered_template
 
 
-def test_informacoes_arquivo_enviado(engine, client):
+def test_informacoes_arquivo_enviado(template, client):
     """Testa se o template exibe as informações do arquivo enviado"""
 
-    template = engine.get_template(
-        template_name='gestao/diligencia/diligencia.html', dirs=engine.dirs
-    )
     context = Context({'nome_arquivo': 'lei_sistema_para.pdf', 'data_envio': '10/08/2017', 'ente_federado': 'Pará'})
     rendered_template = template.render(context)
 
@@ -65,26 +66,55 @@ def test_informacoes_arquivo_enviado(engine, client):
     assert context['ente_federado'] in rendered_template
 
 
-def test_titulo_bloco_informacoes_arquivo(engine, client):
+def test_titulo_bloco_informacoes_arquivo(template, client):
     """Testa se o título Informações sobre o Arquivo Enviado está dentre de uma tag de título h2"""
 
-    template = engine.get_template(
-        template_name='gestao/diligencia/diligencia.html', dirs=engine.dirs
-    )
     rendered_template = template.render(Context({}))
 
     assert "<h2>Informações sobre o Arquivo Enviado</h2>" in rendered_template
 
 
-def test_formatacao_informacoes_sobre_arquivo_enviado(engine, client):
+def test_formatacao_informacoes_sobre_arquivo_enviado(template, client):
     """Testa se cada informação sobre o arquivo enviado está formatada com a tag <p>"""
 
-    template = engine.get_template(
-        template_name='gestao/diligencia/diligencia.html', dirs=engine.dirs
-    )
     context = Context({'nome_arquivo': 'lei_sistema_para.pdf', 'data_envio': '10/08/2017', 'ente_federado': 'Pará'})
-    rendered_template=template.render(context)
+    rendered_template = template.render(context)
 
     assert "<p>{}</p>".format(context['nome_arquivo']) in rendered_template
     assert "<p>{}</p>".format(context['data_envio']) in rendered_template
     assert "<p>{}</p>".format(context['ente_federado']) in rendered_template
+
+
+def test_opcoes_de_classificacao_da_diligencia(template, client):
+    """Testa se a Classificação(Motivo) apresenta as opções conforme a especificação."""
+
+    opcoes = [
+        {"description": "Arquivo Danificado", "value": "arquivo_danificado"},
+        {"description": "Arquivo Incompleto", "value": "arquivo_incompleto"},
+        {"description": "Arquivo Incorreto", "value": "arquivo_incorreto"}
+    ]
+    
+    context = Context({"classificacoes": opcoes})
+    rendered_template = template.render(context)
+
+    assert opcoes[0]['description'] in rendered_template
+    assert opcoes[1]['description'] in rendered_template
+    assert opcoes[2]['description'] in rendered_template
+
+
+def test_opcoes_em_um_dropdown(template, client):
+    """Testa se as Classificações(Motivo) estão presentes dentro de um dropdown."""
+
+    opcoes = [
+        {"description": "Arquivo Danificado", "value": "arquivo_danificado"},
+        {"description": "Arquivo Incompleto", "value": "arquivo_incompleto"},
+        {"description": "Arquivo Incorreto", "value": "arquivo_incorreto"}
+    ]
+    
+    context = Context({"classificacoes": opcoes})
+    rendered_template = template.render(context)
+
+    assert "<select>" in  rendered_template
+    for opcao in opcoes:
+        assert "<option value=\"{value}\">{description}</option>".format(value=opcao['value'], description=opcao['description'])
+    assert "</select>" in rendered_template
