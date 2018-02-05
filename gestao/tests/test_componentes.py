1e9428bb2c7428c6b920a77eed9fe7aae479e58f
André "decko" de Brito <brito.afa@gmail.com>
2018-02-04 22:28:21 -0200

    [WIP] Altera o template e adiciona uma fixture para o teste.

--- gestao/tests/test_componentes.py
+++ gestao/tests/test_componentes.py
@@ -5,34 +5,46 @@ from django.template.base import TemplateDoesNotExist
 
 pytestmark = pytest.mark.django_db
 
-def test_existencia_template_diligencia(client):
-    """ Testando existência do template para criação da diligência"""
+
+@pytest.fixture
+def engine():
+    """ Configura a engine de Templates do Django """
 
     engine = Engine.get_default()
+
+    return engine
+
+
+def test_existencia_template_diligencia(engine, client):
+    """ Testando existência do template para criação da diligência"""
+
     try:
         template = engine.get_template(template_name='gestao/diligencia/diligencia.html', dirs=engine.dirs)
     except TemplateDoesNotExist:
         template = ''
 
     assert isinstance(template, Template)
-    
-def test_botoes_acao_diligencia_template(client):
-    """Testa existencia dos botões de enviar e cancelar no template de diligência"""
 
-    engine = Engine.get_default()
 
-    template = engine.get_template(template_name='gestao/diligencia/diligencia.html', dirs=engine.dirs)
-    rendered_template = template.render(Context())
+def test_botoes_acao_diligencia_template(engine, client):
+    """Testa existencia dos botões de enviar e cancelar
+    no template de diligência"""
 
-    # self.assetInHTML("btn-primary" ", rendered_template)
+    template = engine.get_template(
+        template_name='gestao/diligencia/diligencia.html', dirs=engine.dirs)
+    rendered_template = template.render(Context({}))
+
+    # __import__('ipdb').set_trace()
     assert "<button class='btn btn-primary'>Enviar</button>" in rendered_template
     assert "<button class='btn btn-secondary'>Cancelar</button>" in rendered_template
 
-def test_gestao_template(client):
+
+def test_gestao_template(engine, client):
     """Testa se o template da gestão está sendo carregado"""
 
-    engine = Engine.get_default()
+    template = engine.get_template(
+        template_name='gestao/diligencia/diligencia.html', dirs=engine.dirs)
 
-    template = engine.get_template(template_name='gestao/diligencia/diligencia.html', dirs=engine.dirs)
+    rendered_template = template.render(Context({}))
 
-    assert template.nodelist[0] in 'gestao/base_gestao.html'
+    assert "<!DOCTYPE html>" in rendered_template
