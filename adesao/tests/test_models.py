b40ea80c99e2b0d70716aea7540527e89862c7d5
André "decko" de Brito <brito.afa@gmail.com>
2018-06-05 17:11:22 -0300

    Testa se SistemaCultura tem a propriedade cadastrador.

--- adesao/tests/test_models.py
+++ adesao/tests/test_models.py
@@ -1,8 +1,16 @@
 import pytest
 
+from adesao.models import SistemaCultura
 
 @pytest.mark.xfail
 def test_existe_um_model_SistemaCultura():
 
     with pytest.raises(ImportError):
         from adesao.models import SistemaCultura
+
+
+def test_atributo_cadastrador_de_um_SistemaCultura():
+
+    sistema = SistemaCultura()
+
+    assert sistema._meta.get_field('cadastrador')
