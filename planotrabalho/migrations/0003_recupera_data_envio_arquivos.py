02ca61b2902edd8c699c16b891612ef0a8b9246d
André "decko" de Brito <brito.afa@gmail.com>
2018-06-21 14:58:33 -0300

    Adiciona migration para recuperar a data de modificação de cada arquivo e salvar como data de envio.

---
+++ planotrabalho/migrations/0003_recupera_data_envio_arquivos.py
@@ -0,0 +1,41 @@
+from os import stat
+
+from datetime import datetime
+
+from django.db import migrations, models
+
+
+def recupera_data(apps):
+
+    PlanoTrabalho = apps.get_model("planotrabalho", "PlanoTrabalho")
+    componentes = (
+        "criacao_sistema",
+        "orgao_gestor",
+        "conselho_cultural",
+        "fundo_cultura",
+        "plano_cultura",
+    )
+    planos = PlanoTrabalho.objects.filter(
+        plano_cultura__isnull=False
+    ).prefetch_related()
+
+    for plano in planos:
+        for componente in componentes:
+            try:
+                componente_plano = getattr(plano, componente)
+                arquivo = componente_plano.arquivo.file
+                stats = stat(str(arquivo))
+                data = datetime.fromtimestamp(stats.st_mtime)
+                componente_plano.data_envio = data
+                componente_plano.save()
+            except:
+                pass
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [("planotrabalho", "0002_migra_arquivos_componentes")]
+
+    operations = [
+        migrations.RunPython(recupera_data),
+    ]
