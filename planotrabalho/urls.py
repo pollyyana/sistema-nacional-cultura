102928cf724929adab30d9daf27eebc6da5f8d8e
André "decko" de Brito <brito.afa@gmail.com>
2018-08-22 06:55:22 -0300

    Altera o namespace da view CadastrarSistema. Formata o arquivo usando Black.

--- planotrabalho/urls.py
+++ planotrabalho/urls.py
@@ -3,67 +3,83 @@ from django.contrib.auth.decorators import login_required
 
 from . import views
 
-app_name = 'planotrabalho'
+app_name = "planotrabalho"
 
 urlpatterns = [
-    url(r'^(?P<pk>[0-9]+)/$',
+    url(
+        r"^(?P<pk>[0-9]+)/$",
         login_required(views.PlanoTrabalho.as_view()),
-        name='detail'),
-
+        name="detail",
+    ),
     # Primeira etapa - Criação do Sistema de Cultura
-    url(r'^sistema/$',
-        login_required(views.CadastrarSistema.as_view()),
-        name='sistema'),
-    url(r'^sistema/(?P<pk>[0-9]+)/$',
+    url(
+        r"^sistema/$", login_required(views.CadastrarSistema.as_view()),
+        name="cadastrar_sistema"
+    ),
+    url(
+        r"^sistema/(?P<pk>[0-9]+)/$",
         login_required(views.AlterarSistema.as_view()),
-        name='alterar_sistema'),
-
+        name="alterar_sistema",
+    ),
     # Segunda etapa - Estruturação do Orgão Gestor
-    url(r'^gestor/$',
+    url(
+        r"^gestor/$",
         login_required(views.CadastrarOrgaoGestor.as_view()),
-        name='gestor'),
-    url(r'^gestor/(?P<pk>[0-9]+)/$',
+        name="gestor",
+    ),
+    url(
+        r"^gestor/(?P<pk>[0-9]+)/$",
         login_required(views.AlterarOrgaoGestor.as_view()),
-        name='alterar_gestor'),
-
+        name="alterar_gestor",
+    ),
     # Terceira etapa - Criação do Conselho de Política Cultural
-    url(r'^conselho/$',
+    url(
+        r"^conselho/$",
         login_required(views.CadastrarConselho.as_view()),
-        name='conselho'),
-    url(r'^conselho/(?P<pk>[0-9]+)/$',
+        name="conselho",
+    ),
+    url(
+        r"^conselho/(?P<pk>[0-9]+)/$",
         login_required(views.AlterarConselho.as_view()),
-        name='alterar_conselho'),
-    url(r'^conselheiros/$',
+        name="alterar_conselho",
+    ),
+    url(
+        r"^conselheiros/$",
         login_required(views.get_conselheiros),
-        name='get_conselheiros'),
-    url(r'^conselheiro/criar/$',
+        name="get_conselheiros",
+    ),
+    url(
+        r"^conselheiro/criar/$",
         login_required(views.CriarConselheiro.as_view()),
-        name="criar_conselheiro"),
-    url(r'^conselheiro/listar/$',
+        name="criar_conselheiro",
+    ),
+    url(
+        r"^conselheiro/listar/$",
         login_required(views.ListarConselheiros.as_view()),
-        name="listar_conselheiros"),
-    url(r'^conselheiro/editar/(?P<pk>[0-9]+)/$',
+        name="listar_conselheiros",
+    ),
+    url(
+        r"^conselheiro/editar/(?P<pk>[0-9]+)/$",
         login_required(views.AlterarConselheiro.as_view()),
-        name="alterar_conselheiro"),
-    url(r'^conselheiro/remover/(?P<pk>[0-9]+)/$',
+        name="alterar_conselheiro",
+    ),
+    url(
+        r"^conselheiro/remover/(?P<pk>[0-9]+)/$",
         login_required(views.DesabilitarConselheiro.as_view()),
-        name="remover_conselheiro"),
-
-
-
+        name="remover_conselheiro",
+    ),
     # Quarta etapa - Criação do Fundo de Cultura
-    url(r'^fundo/$',
-        login_required(views.CadastrarFundo.as_view()),
-        name='fundo'),
-    url(r'^fundo/(?P<pk>[0-9]+)/$',
+    url(r"^fundo/$", login_required(views.CadastrarFundo.as_view()), name="fundo"),
+    url(
+        r"^fundo/(?P<pk>[0-9]+)/$",
         login_required(views.AlterarFundo.as_view()),
-        name='alterar_fundo'),
-
+        name="alterar_fundo",
+    ),
     # Quinta etapa - Elaboração do Plano de Cultura
-    url(r'^plano/$',
-        login_required(views.CadastrarPlano.as_view()),
-        name='plano'),
-    url(r'^plano/(?P<pk>[0-9]+)/$',
+    url(r"^plano/$", login_required(views.CadastrarPlano.as_view()), name="plano"),
+    url(
+        r"^plano/(?P<pk>[0-9]+)/$",
         login_required(views.AlterarPlano.as_view()),
-        name='alterar_plano'),
-    ]
+        name="alterar_plano",
+    ),
+]
