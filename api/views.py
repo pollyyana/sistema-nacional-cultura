2f1a77845031aef3c138d4a8d47824f5052d5a72
Izabela Cardoso <cizabelacristina@gmail.com>
2018-08-22 16:47:07 -0300

    Adicionando aggregate com counts

--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Q, Count
 
 from rest_framework import filters
 from rest_framework import generics
+from rest_framework.response import Response
 
 from adesao.models import Municipio
 from planotrabalho.models import PlanoTrabalho
@@ -20,12 +22,26 @@ def swagger_index(request):
 
 class MunicipioList(generics.ListAPIView):
     queryset = Municipio.objects.filter().order_by('-id')
+    extra_counts = queryset.aggregate(
+        cidades=Count('pk', filter=Q(cidade__isnull=False)),
+        estados=Count('pk', filter=Q(cidade__isnull=True)),
+        estados_aderidos=Count('pk', filter=(Q(usuario__estado_processo=6) & Q(cidade__isnull=True))),
+        municipios_aderidos=Count('pk', filter=(Q(usuario__estado_processo=6) & Q(cidade__isnull=False))),
+    )
     serializer_class = MunicipioSerializer
     metadata_class = MunicipioMetadata
 
     filter_backends = (DjangoFilterBackend, filters.OrderingFilter,)
     filter_class = MunicipioFilter
     ordering_fields = ('cidade__nome_municipio', 'estado__nome_uf')
+
+    def list(self, request):
+        response = super(MunicipioList, self).list(self, request)
+        response.data['cidades'] = self.extra_counts['cidades']
+        response.data['estados'] = self.extra_counts['estados']
+        response.data['estados_aderidos'] = self.extra_counts['estados_aderidos']
+        response.data['municipios_aderidos'] = self.extra_counts['municipios_aderidos']
+        return response
 
 
 class MunicipioDetail(generics.RetrieveAPIView):
