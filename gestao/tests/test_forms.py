77bf9828e30a2b10f0332329b611724295b01c4c
André "decko" de Brito <andre.brito@lablivre.wiki.br>
2018-10-11 16:34:00 -0300

    Altera os testes para usarem fixtures.

--- gestao/tests/test_forms.py
+++ gestao/tests/test_forms.py
@@ -14,90 +14,78 @@ from gestao.forms import AlterarCadastradorForm
 pytestmark = pytest.mark.django_db
 
 
-@pytest.fixture
-def usuario():
-
-    return mommy.make("Usuario")
-
-
-@pytest.fixture
-def sistema_cultura():
-
-    return mommy.make("SistemaCultura")
-
-
-def test_existencia_form_diligencia_componente(client, usuario, sistema_cultura):
+def test_existencia_form_diligencia_componente(client, login, sistema_cultura):
     """ Testa existência da classe form para a diligência de componentes"""
 
     form = DiligenciaComponenteForm(componente='orgao_gestor', sistema_cultura=sistema_cultura,
-        usuario=usuario)
+        usuario=login)
 
     assert form
 
 
-def test_existencia_form_diligencia_geral(client, usuario, sistema_cultura):
+def test_existencia_form_diligencia_geral(client, login, sistema_cultura):
     """ Testa existência da classe form para a diligência geral"""
 
-    form = DiligenciaGeralForm(sistema_cultura=sistema_cultura, usuario=usuario)
+    form = DiligenciaGeralForm(sistema_cultura=sistema_cultura, usuario=login)
 
     assert form
 
 
-def test_campo_texto_diligencia_form_componente(client, usuario, sistema_cultura):
+def test_campo_texto_diligencia_form_componente(client, login, sistema_cultura):
     """
     Testa existência do campo texto_diligência no form referente a diligência de componente
     """
 
     form = DiligenciaComponenteForm(componente='orgao_gestor', sistema_cultura=sistema_cultura,
-        usuario=usuario)
+        usuario=login)
 
     assert "<textarea cols=\"40\" id=\"id_texto_diligencia\" name=\"texto_diligencia\" " in form.as_p()
 
 
-def test_campo_texto_diligencia_form_geral(client, usuario, sistema_cultura):
+def test_campo_texto_diligencia_form_geral(client, login, sistema_cultura):
     """
     Testa existência do campo texto_diligência no form referente a diligência geral
     """
 
-    form = DiligenciaGeralForm(sistema_cultura=sistema_cultura, usuario=usuario)
+    form = DiligenciaGeralForm(sistema_cultura=sistema_cultura, usuario=login)
     
     assert "<textarea cols=\"40\" id=\"id_texto_diligencia\" name=\"texto_diligencia\" " in form.as_p()
 
 
-def test_campo_classificao_arquivo_no_form_diligencia_componente(client, usuario, sistema_cultura):
+def test_campo_classificao_arquivo_no_form_diligencia_componente(client, login, sistema_cultura):
     """ Testa a existência do campo referente a seleção para a classificação do arquivo """
 
     form = DiligenciaComponenteForm(componente='orgao_gestor', sistema_cultura=sistema_cultura,
-        usuario=usuario)
+        usuario=login)
 
     assert "<select name=\"classificacao_arquivo\" id=\"id_classificacao_arquivo\"" in form.as_p()
 
 
-def test_ausencia_campo_classificao_arquivo_no_form_diligencia_geral(client, usuario, sistema_cultura):
+def test_ausencia_campo_classificao_arquivo_no_form_diligencia_geral(client, login, sistema_cultura):
     """ Testa se o campo referente a seleção para a classificação do arquivo
     não é disonível na diligência geral """
 
-    form = DiligenciaGeralForm(sistema_cultura=sistema_cultura, usuario=usuario)
+    form = DiligenciaGeralForm(sistema_cultura=sistema_cultura, usuario=login)
 
     assert "<select name=\"classificacao_arquivo\" id=\"id_classificacao_arquivo\"" not in form.as_p()
 
 
-def test_uso_ck_widget_no_texto_diligencia_geral(client, usuario, sistema_cultura):
+def test_uso_ck_widget_no_texto_diligencia_geral(client, login, sistema_cultura):
     """ Testa uso do widget ckeditor para input de texto rich no texto_diligência """
 
-    form = DiligenciaGeralForm(sistema_cultura=sistema_cultura, usuario=usuario)
+    form = DiligenciaGeralForm(sistema_cultura=sistema_cultura, usuario=login)
 
     assert isinstance(form.fields['texto_diligencia'].widget, CKEditorWidget)
 
 
-def test_validacao_de_dados_invalidos(client, usuario, sistema_cultura):
+def test_validacao_de_dados_invalidos(client, login, sistema_cultura):
     """ Testa se a função is_valid retorna falso para dados inválidos na criação do form 
     da diligencia de componente"""
 
     data = {'texto_diligencia': 'ta certo, parceiro', 'classificacao_arquivo': 'bla'}
 
     form = DiligenciaComponenteForm(data=data, componente='orgao_gestor', 
-        sistema_cultura=sistema_cultura, usuario=usuario)
+        sistema_cultura=sistema_cultura, usuario=login)
 
     assert not form.is_valid()
 
@@ -114,38 +102,38 @@ def test_tipo_do_form_da_diligencia_componente(client):
     assert issubclass(DiligenciaComponenteForm, ModelForm)
 
 
-def test_diligencia_form_componente_usa_model_correta(client, usuario, sistema_cultura):
+def test_diligencia_form_componente_usa_model_correta(client, login, sistema_cultura):
     """ Testa de a classe DiligenciaForm utiliza a model referente a Diligencia """
 
     form = DiligenciaComponenteForm(componente='orgao_gestor', 
-        sistema_cultura=sistema_cultura, usuario=usuario)
+        sistema_cultura=sistema_cultura, usuario=login)
 
     assert isinstance(form.instance, DiligenciaSimples)
 
 
-def test_diligencia_form_geral_usa_model_correta(client, usuario, sistema_cultura):
+def test_diligencia_form_geral_usa_model_correta(client, login, sistema_cultura):
     """ Testa de a classe DiligenciaForm utiliza a model referente a Diligencia """
 
-    form = DiligenciaGeralForm(sistema_cultura=sistema_cultura, usuario=usuario)
+    form = DiligenciaGeralForm(sistema_cultura=sistema_cultura, usuario=login)
 
     assert isinstance(form.instance, DiligenciaSimples)
 
 
-def test_fields_form_diligencia_componente(client, usuario, sistema_cultura):
+def test_fields_form_diligencia_componente(client, login, sistema_cultura):
     """Testa as fields dentro do form de diligencia de componente"""
 
     form = DiligenciaComponenteForm(componente='orgao_gestor', 
-        sistema_cultura=sistema_cultura, usuario=usuario)
+        sistema_cultura=sistema_cultura, usuario=login)
 
     fields = ('texto_diligencia', 'classificacao_arquivo')
 
     assert set(form.Meta.fields).issuperset(set(fields))
 
 
-def test_fields_form_diligencia_geral(client, usuario, sistema_cultura):
+def test_fields_form_diligencia_geral(client, login, sistema_cultura):
     """Testa as fields dentro do form de diligencia geral"""
 
-    form = DiligenciaGeralForm(sistema_cultura=sistema_cultura, usuario=usuario)
+    form = DiligenciaGeralForm(sistema_cultura=sistema_cultura, usuario=login)
     
     fields = ('texto_diligencia',)
 
@@ -175,64 +163,17 @@ def test_campos_form_altera_cadastrador(client):
     assert set(form.Meta.fields) == set(fields)
 
 
-def test_widget_estado_form_alterar_cadastrador(client):
-    """
-    Testa o uso do widget ModelSelect2 na campo estado no form de
-    alterar cadastrador
-    """
-
-    form = AlterarCadastradorForm()
-    assert isinstance(form['estado'].field.widget, ModelSelect2)
-
-
-def test_url_widget_estado_form_alterar_cadastrador(client):
-    """
-    Testa url usada pelo widget ModelSelect2 no campo estado no form
-    de alterar cadastrador
-    """
-
-    form = AlterarCadastradorForm()
-    uf_url = reverse('gestao:uf_chain')
-
-    assert form['estado'].field.widget.url == uf_url
-
-
-def test_widget_municipio_form_alterar_cadastrador(client):
-    """
-    Testa o uso do widget ModelSelect2 no campo municipio no form de
-    alterar cadastrador
-    """
-
-    form = AlterarCadastradorForm()
-    assert isinstance(form['municipio'].field.widget, ModelSelect2)
-
-
-def test_url_widget_municipio_form_alterar_cadastrador(client):
-    """
-    Testa url usada pelo widget ModelSelect2 no campo municipio no
-    form de alterar cadastrador
-    """
-
-    form = AlterarCadastradorForm()
-    municipio_url = reverse('gestao:cidade_chain')
-    assert form['municipio'].field.widget.url == municipio_url
-
-
-def test_save_alterar_cadastrador_form_com_sistemacultura(plano_trabalho):
+@pytest.skip("Deve acompanhar a refatoração do processo de alterar cadastrador")
+def test_save_alterar_cadastrador_form_com_sistemacultura(sistema_cultura):
     """
     Método save do form AlterarCadastradorForm altera as informações necessárias,
     quando um ente fedarado já possui um SistemaCultura associado
     """
-    cidade = mommy.make('Cidade')
-    municipio = mommy.make('Municipio', cidade=cidade, estado=cidade.uf)
-    user = mommy.make('Usuario', municipio=municipio)
-    new_user = mommy.make('Usuario', user__username='23012616039')
 
-    mommy.make('SistemaCultura', cadastrador=user, uf=municipio.estado,
-               cidade=municipio.cidade)
     data = {'cpf_usuario': new_user.user.username,
             'estado': municipio.estado.codigo_ibge,
             'municipio': municipio.cidade.id}
+
     form = AlterarCadastradorForm(data=data)
     form.is_valid()
     form.save()
@@ -240,44 +181,3 @@ def test_save_alterar_cadastrador_form_com_sistemacultura(plano_trabalho):
     municipio.refresh_from_db()
 
     assert municipio.usuario == new_user
-
-
-def test_save_alterar_cadastrador_form_sem_sistemacultura(plano_trabalho):
-    """
-    Método save do form AlterarCadastradorForm altera as informações necessárias,
-    quando um ente fedarado NÂO possui um SistemaCultura associado
-    """
-    cidade = mommy.make('Cidade')
-    municipio = mommy.make('Municipio', cidade=cidade, estado=cidade.uf)
-    mommy.make('Usuario', municipio=municipio)
-    new_user = mommy.make('Usuario', user__username='23012616039')
-
-    data = {'cpf_usuario': new_user.user.username,
-            'estado': municipio.estado.codigo_ibge,
-            'municipio': municipio.cidade.id}
-    form = AlterarCadastradorForm(data=data)
-    form.is_valid()
-    form.save()
-
-    municipio.refresh_from_db()
-
-    assert municipio.usuario == new_user
-
-
-def test_save_alterar_cadastrador_form_sem_municipio(plano_trabalho):
-    """
-    Testa form de alteração de cadastrador no caso que não existe municipío
-    """
-
-    estado = mommy.make('Uf')
-    cidade = mommy.make('Cidade', uf=estado)
-    new_user = mommy.make('Usuario', user__username='23012616039')
-
-    data = {'cpf_usuario': new_user.user.username,
-            'estado': estado.codigo_ibge,
-            'municipio': cidade.id}
-    form = AlterarCadastradorForm(data=data)
-    form.is_valid()
-    sistema = form.save()
-
-    assert sistema.cadastrador == new_user
