9edd886ed6e14f0d8bf6bed6c9a50d2de625c3e7
André "decko" de Brito <andre.brito@lablivre.wiki.br>
2018-10-24 15:25:45 -0300

    Ajusta as URLs para usar debug toolbar quando em desenvolvimento.

--- snc/urls.py
+++ snc/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
 
-from django.urls import include
+from django.urls import include, path
 
 from django.contrib.auth.views import PasswordResetView
 from django.contrib.auth.views import PasswordResetDoneView
@@ -76,3 +76,13 @@ urlpatterns = [
     url(r'^api/', include('api.urls', namespace='api')),
 
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+
+        # For django versions before 2.0:
+        # url(r'^__debug__/', include(debug_toolbar.urls)),
+
+    ] + urlpatterns
