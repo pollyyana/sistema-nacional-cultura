e9d951515276db936a328ee40eb461732dba3437
Gabriel Avaloni <gabrielavaloni@gmail.com>
2018-12-04 18:44:05 -0200

    Correção dos paths da URL
    Co-authored-by: Pedro <pedrophilipe.ti@gmail.com>

--- apiv2/urls.py
+++ apiv2/urls.py
@@ -7,13 +7,13 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
-    path('/', views.swagger_index, name='swagger-index'),
+    path('', views.swagger_index, name='swagger-index'),
 
-    path('/v2/sistemadeculturalocal/', views.SistemaCulturaAPIList.as_view(), name='sistemacultura-list'),
-    path('/v2/sistemadeculturalocal/<int:pk>/', views.MunicipioDetail.as_view(), name='sistemacultura-detail'),
+    path('v2/sistemadeculturalocal/', views.SistemaCulturaAPIList.as_view(), name='sistemacultura-list'),
+    path('v2/sistemadeculturalocal/<int:pk>/', views.MunicipioDetail.as_view(), name='sistemacultura-detail'),
 
-    path('/v2/acoesplanotrabalho/', views.PlanoTrabalhoList.as_view(), name='planotrabalho-list'),
-    path('/v2/acoesplanotrabalho/<int:pk>/', views.PlanoTrabalhoDetail.as_view(), name='planotrabalho-detail'),
+    path('v2/acoesplanotrabalho/', views.PlanoTrabalhoList.as_view(), name='planotrabalho-list'),
+    path('v2/acoesplanotrabalho/<int:pk>/', views.PlanoTrabalhoDetail.as_view(), name='planotrabalho-detail'),
 
 ]
 
