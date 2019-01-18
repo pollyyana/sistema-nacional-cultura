664bbc6a8557c5fff2c23cfe160745c79b330219
Gabriel Avaloni <gabrielavaloni@gmail.com>
2019-01-18 16:12:18 -0200

    Retira dependência do pandas no script de importação

--- planilha.py
+++ planilha.py
@@ -1,5 +1,6 @@
 import os, sys
 import environ
+import csv
 import django
 import pandas as pd
 
@@ -46,75 +47,53 @@ UFS = {
     "DISTRITO FEDERAL": 53
 }
 
-df = pd.read_csv("planilha.csv")
+with open("adesao/fixtures/planilha.csv") as file:
+    i = csv.DictReader(file)
 
-criados = []
-publicados = []
-removidos = []
+    for df in i:
+        sistemas = None
+        sistema = None
 
-for i in range(0, len(df)):
-    # import pdb; pdb.set_trace()
-    sistemas = None
-    sistema = None
+        ente = df['ente'].strip()
+        estado_do_processo = df['estado_do_processo']
+        data_adesao = df['data_adesao']
 
-    ente = df['ente'][i].strip()
-    estado_do_processo = df['estado_do_processo'][i]
-    data_adesao = df['data_adesao'][i]
+        if estado_do_processo == estado_do_processo:
+            estado_do_processo = estado_do_processo.strip()
 
-    if estado_do_processo == estado_do_processo:
-        estado_do_processo = estado_do_processo.strip()
+        if data_adesao == data_adesao:
+            data_adesao = data_adesao.strip()
 
-    if data_adesao == data_adesao:
-        data_adesao = data_adesao.strip()
-
-    if ente.isupper():
-        codigo_ibge = UFS[ente]
-
-    # if ente == "Passagem":
-    #     import pdb; pdb.set_trace()
-
-    sistemas = SistemaCultura.sistema.filter(ente_federado__nome__unaccent__iexact=ente, ente_federado__cod_ibge__startswith=codigo_ibge)
-
-    if len(sistemas) > 1:
         if ente.isupper():
-            sistema = sistemas.filter(ente_federado__cod_ibge=codigo_ibge)[0]
-        else:
-            sistema = sistemas.exclude(ente_federado__cod_ibge=codigo_ibge)[0]
-    elif len(sistemas) == 0:
-        if estado_do_processo in ('1', 'Sim'):
+            codigo_ibge = UFS[ente]
+            nome_estado = ente
+
+        sistemas = SistemaCultura.sistema.filter(ente_federado__nome__unaccent__iexact=ente, ente_federado__cod_ibge__startswith=codigo_ibge)
+
+        if len(sistemas) > 1:
+            if ente.isupper():
+                sistema = sistemas.filter(ente_federado__cod_ibge=codigo_ibge)[0]
+            else:
+                sistema = sistemas.exclude(ente_federado__cod_ibge=codigo_ibge)[0]
+        elif len(sistemas) == 0:
             try:
                 entefederado = EnteFederado.objects.get(nome__unaccent__iexact=ente, cod_ibge__startswith=codigo_ibge)
             except EnteFederado.DoesNotExist:
-                print(f"{ente} - {codigo_ibge}")
+                print(f"{ente} - {UFS[nome_estado]} - Planilha: {estado_do_processo}")
                 continue
 
             sistema = SistemaCultura()
             sistema.ente_federado = entefederado
-            criados.append(sistema.ente_federado.nome + " " + str(sistema.ente_federado.cod_ibge))
         else:
-            continue
-    else:
-        sistema = sistemas[0]
+            sistema = sistemas[0]
 
-    # if sistema.ente_federado.nome == 'Rio Branco':
-    #     import pdb; pdb.set_trace()
-    
-    if estado_do_processo in ('1', 'Sim'):
-        sistema.estado_processo = '6'
-        data_adesao = dt.strptime(data_adesao, '%d/%m/%Y')
-        sistema.data_publicacao_acordo = data_adesao
-        publicados.append(sistema.ente_federado.nome + " " + str(sistema.ente_federado.cod_ibge))
-    else:
-        if sistema.estado_processo == '6':
-            sistema.estado_processo = '0'
-            sistema.data_publicacao_acordo = None
-            removidos.append(sistema.ente_federado.nome + " " + str(sistema.ente_federado.cod_ibge))
+        if estado_do_processo in ('1', 'Sim'):
+            sistema.estado_processo = '6'
+            data_adesao = dt.strptime(data_adesao, '%d/%m/%Y')
+            sistema.data_publicacao_acordo = data_adesao
+        else:
+            if sistema.estado_processo == '6':
+                sistema.estado_processo = '0'
+                sistema.data_publicacao_acordo = None
 
-    sistema.save()
-
-banco = []
-for item in SistemaCultura.sistema.filter(estado_processo=6):
-    banco.append(item.ente_federado.nome + " " + str(item.ente_federado.cod_ibge))
-
-import pdb; pdb.set_trace()
-                    
+        sistema.save()
