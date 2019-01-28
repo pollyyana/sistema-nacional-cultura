74b7979363e8f2bbd103bda2f9a4f3bd0d7ec48b
Izabela Cardoso <izabela.cardoso@lablivre.wiki.br>
2019-01-28 15:26:00 -0200

    Adiciona middleware para obter usuario atual

---
+++ adesao/middleware.py
@@ -0,0 +1,18 @@
+from threading import local
+
+_thread_locals = local()
+
+class ThreadLocalUserMiddleware(object):
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        _thread_locals.request = request
+        response = self.get_response(request)
+        return response
+
+def get_current_user():
+    request = getattr(_thread_locals, "request", None)
+    if request:
+        return getattr(request, "user", None)
