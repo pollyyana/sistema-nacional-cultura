f9a8ed87ffe6122267ba0a5e631a4e2cd332116e
Izabela Cardoso <izabela.cardoso@lablivre.wiki.br>
2019-02-15 15:17:30 -0200

    Cria campo para participação na conferência nacional

--- adesao/models.py
+++ adesao/models.py
@@ -403,6 +403,7 @@ class SistemaCultura(models.Model):
     justificativa = models.TextField(_("Justificativa"), blank=True, null=True)
     diligencia = models.ForeignKey("gestao.DiligenciaSimples", on_delete=models.SET_NULL, related_name="sistema_cultura", blank=True, null=True)
     prazo = models.IntegerField(default=2)
+    conferencia_nacional = models.BooleanField(blank=True)
     alterado_em = models.DateTimeField("Alterado em", default=timezone.now)
     alterado_por = models.ForeignKey("Usuario", on_delete=models.SET_NULL, null=True, related_name="sistemas_alterados")
 
